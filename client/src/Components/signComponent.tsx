2c3b332abb57146d211bbb043d2a1d9efab5f8d4
dogyeong <82639552+yeomdogyeong@users.noreply.github.com>
2023-06-19 22:19:52 +0900

    [design]회원가입 css틀

--- client/src/Components/signComponent.tsx
+++ client/src/Components/signComponent.tsx
@@ -2,9 +2,34 @@ import React from "react";
 
 const SignComponent = () => {
   return (
-    <div className="bg-white shadow-lg rounded-lg mx-auto w-3/5 p-4 border border-black">
-      <div className="text-red-500">hi~</div>
-    </div>
+    <>
+      <div className="bg-white shadow-custom mx-auto flex flex-col  space-y-3 w-full h-full sm:w-full sm:h-full md:w-[600px] md:h-[500px] p-4 border border-#e0e0e0 rounded-3xl">
+        <div className="flex-2 text-gray-300 font-semibold">펫칭</div>
+        <div className="flex-2 text-left text-gray-300">아이디</div>
+        <div className="flex w-full">
+          <input className="flex-1 border border-gray-300 p-2 rounded-lg mr-2" />
+          <button className="flex-2 border border-gray-300 text-center text-black rounded-lg p-2">
+            중복확인
+          </button>
+        </div>
+
+        <div className="flex-2 text-left text-gray-300">비밀번호</div>
+        <input className="flex-2 border border-gray-300 p-2 rounded-lg" />
+        <div className="flex-2 text-left text-gray-300">비밀번호 확인</div>
+        <input className="flex-2 border border-gray-300 p-2 rounded-lg" />
+        <div className="flex-2 text-left text-gray-300">닉네임</div>
+        <div className="flex w-full">
+          <input className="flex-1 border border-gray-300 p-2 rounded-lg mr-2" />
+          <button className="flex-2 border border-gray-300 text-center text-black rounded-lg p-2">
+            중복확인
+          </button>
+        </div>
+        <br></br>
+        <button className="flex-2 bg-customGreen border border-gray-300 p-2 rounded text-white">
+          회원가입하기
+        </button>
+      </div>
+    </>
   );
 };
 
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        custom: "0 0 10px 5px rgba(0, 0, 0, 0.1)",
+      },
+      colors: {
+        customGreen: "#BADA7A",
+      },
+    },
   },
   plugins: [],
 };
