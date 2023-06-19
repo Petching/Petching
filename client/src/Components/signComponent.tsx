24cf0e6f5f51e04e430a3f52e5d52bb5463381c1
dogyeong <82639552+yeomdogyeong@users.noreply.github.com>
2023-06-19 18:36:01 +0900

    [init]tailwind-css 설정완료

---
+++ client/src/Components/signComponent.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SignComponent = () => {
+  return (
+    <div className="bg-white shadow-lg rounded-lg mx-auto w-3/5 p-4 border border-black">
+      <div className="text-red-500">hi~</div>
+    </div>
+  );
+};
+
+export default SignComponent;
---
+++ client/src/Page/signIn.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SignComponent from "../Components/signComponent";
+
+const SignIn = () => {
+  return (
+    <div className="h-screen flex items-center justify-center bg-gray-100">
+      <SignComponent />
+    </div>
+  );
+};
+
+export default SignIn;
