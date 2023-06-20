0e0ae901a3af4ffe3b87b048806117a29e9c0445
dogyeong <82639552+yeomdogyeong@users.noreply.github.com>
2023-06-20 16:18:06 +0900

    [Feat]회원가입 기능구현

--- client/src/Components/signComponent.tsx
+++ client/src/Components/signComponent.tsx
@@ -1,43 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
+import { checkUser } from "../API/checkUser";
 
-const SignComponent = () => {
+const SignComponent: React.FC = () => {
+  const [message, setMessage] = useState("");
+
+  const handleCheck = async () => {
+    // checkUser 함수 호출
+    const data = await checkUser();
+    // 유저 정보 비교
+    if (data.exists) {
+      setMessage("중복입니다");
+    } else {
+      setMessage("사용 가능");
+    }
+  };
+
   return (
     <>
-      <div className="bg-white shadow-custom mx-auto flex flex-col  space-y-3 w-full h-full sm:w-full sm:h-full md:w-[600px] md:h-[700px] p-4 border border-#e0e0e0 rounded-3xl">
+      <div className=" bg-white shadow-custom mx-auto flex flex-col  space-y-3 w-full h-full sm:w-full sm:h-full md:w-[600px] md:h-[700px] p-4 border border-#e0e0e0 rounded-3xl">
         <div className="mt-11 mb-11 text-gray-300 font-semibold">펫칭</div>
-        <div className=" text-left text-gray-300">아이디</div>
-        <div className="flex w-full">
+        <div className="ml-4 text-left text-gray-300">아이디</div>
+        <div className="flex items-center">
           <input
-            className=" border border-gray-300 p-2 rounded-lg mr-2"
+            className="ml-4  border border-gray-300 p-2 rounded-lg mr-2"
             placeholder="이메일을 입력해주세요"
           />
-          <button className="flex-2 border border-gray-300 p-2 rounded text-white bg-customGreen hover:bg-green-500">
+          <button
+            onClick={handleCheck}
+            className="ml-2 mr-9 flex-2 border border-gray-300 p-2 rounded text-white bg-customGreen hover:bg-green-500"
+          >
             중복확인
           </button>
+          <div>{message}</div>
         </div>
 
-        <div className=" text-left text-gray-300">비밀번호</div>
+        <div className="ml-4  text-left text-gray-300">비밀번호</div>
         <input
-          className=" border border-gray-300 p-2 rounded-lg"
+          className="ml-4  mr-7 border border-gray-300 p-2 rounded-lg"
           placeholder="비밀번호를 입력해주세요"
         />
-        <div className=" text-left text-gray-300">비밀번호 확인</div>
+        <div className="ml-4 text-left text-gray-300">비밀번호 확인</div>
         <input
-          className=" border border-gray-300 p-2 rounded-lg"
+          className="ml-4 mr-7 border border-gray-300 p-2 rounded-lg"
           placeholder="비밀번호를 다시 입력해주세요"
         />
-        <div className=" text-left text-gray-300">닉네임</div>
-        <div className="flex w-full">
+        <div className="ml-4  text-left text-gray-300">닉네임</div>
+        <div className="flex w-full items-center">
           <input
-            className=" border border-gray-300 p-2 rounded-lg mr-2"
+            className="ml-4  border border-gray-300 p-2 rounded-lg mr-2"
             placeholder="닉네임을 입력해주세요"
           />
-          <button className="flex-2 border border-gray-300 p-2 rounded text-white bg-customGreen hover:bg-green-500">
+          <button
+            onClick={handleCheck}
+            className="ml-2 mr-9 flex-2 border border-gray-300 p-2 rounded text-white bg-customGreen hover:bg-green-500"
+          >
             중복확인
           </button>
+          <div
+            className={`${
+              message === "중복입니다"
+                ? "text-red-500"
+                : message === "사용 가능"
+                ? "text-blue-500"
+                : "text-black"
+            }`}
+          >
+            {message}
+          </div>
         </div>
         <br></br>
-        <button className="flex-2 bg-customGreen border border-gray-300 p-2 rounded text-white">
+        <button className="ml-4 flex-2 bg-customGreen border border-gray-300 p-2 rounded text-white">
           회원가입하기
         </button>
       </div>
