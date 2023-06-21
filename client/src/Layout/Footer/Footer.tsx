2c782149ca141c77af1d864f52b3a501aed98354
938938 <938kjh@gmail.com>
2023-06-21 21:20:32 +0900

    [design] Footer 기본 UI 설정

---
+++ client/src/Layout/Footer/Footer.tsx
@@ -0,0 +1,23 @@
+const Footer = () => {
+  return (
+    <div className='bg-customGreen h-8 fixed bottom-0 left-0 w-full z-50 flex justify-between items-center px-2'>
+      <div className='w-5'>
+        <img
+          src='https://img.icons8.com/?size=512&id=121198&format=png'
+          alt='발바닥 모양의 아이콘'
+        />
+      </div>
+      <div className='flex items-center'>
+        <button>문의하기</button>
+        <button className='w-5 ml-2'>
+          <img
+            src='https://img.icons8.com/?size=512&id=106562&format=png'
+            alt='깃허브 링크'
+          />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
