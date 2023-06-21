f6284aa9a21616fca6d2a53a006f6280f662356f
dogyeong <82639552+yeomdogyeong@users.noreply.github.com>
2023-06-21 23:15:34 +0900

    [Design]chatlist page구현

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,12 +3,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import SignIn from "./Page/signIn";
 import SignUp from "./Page/signUp";
+import ChatList from "./Page/chatList";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/signup" element={<SignUp />}></Route>
         <Route path="/signin" element={<SignIn />}></Route>
+        <Route path="/chatlist" element={<ChatList />}></Route>
       </Routes>
     </BrowserRouter>
   );
