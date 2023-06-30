84fee023ff5686121f2f378d6bb06f165a1f5a86
sksDud <hinagogo@naver.com>
2023-06-30 17:49:13 +0900

    [CI/CD] 배포 자동화 환경 구축

--- server/src/main/java/com/Petching/petching/board/repository/BoardRepository.java
+++ server/src/main/java/com/Petching/petching/board/repository/BoardRepository.java
@@ -1,14 +1,12 @@
 package com.Petching.petching.board.repository;
 
 import com.Petching.petching.board.entity.Board;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface BoardRepository extends JpaRepository<Board, Long> {
