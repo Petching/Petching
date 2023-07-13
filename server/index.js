a55bc137bacc0a8c3073baa67c98bd498a8c9923
dogyeong <82639552+yeomdogyeong@users.noreply.github.com>
2023-07-13 17:32:18 +0900

    [etc]server chatting room 기능 추가

--- server/index.js
+++ server/index.js
@@ -15,8 +15,22 @@ const io = new Server(server, {
   },
 });
 
+const rooms = {};
+
 io.on("connection", (socket) => {
   console.log("New client connected");
+
+  socket.on("createRoom", (room) => {
+    // 채팅방이 이미 존재하는 경우
+    if (rooms[room]) {
+      socket.emit("notification", `Room ${room} already exists`);
+    } else {
+      // 새로운 채팅방을 생성하고, 사용자를 참여시킴
+      rooms[room] = true;
+      socket.join(room);
+      socket.emit("notification", `Room ${room} created and joined`);
+    }
+  });
 
   socket.on("joinRoom", (room) => {
     socket.join(room);
