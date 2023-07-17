f9dfb254b32581cdf39bf52a9a8c5160a34e4ee6
dogyeong <82639552+yeomdogyeong@users.noreply.github.com>
2023-07-17 17:09:48 +0900

    [refactor]tsconfig.json 경로 오류 해결코드 추가

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import { RouterProvider } from 'react-router-dom';
 import './App.css';
 import { routers } from './router';
+import React from 'react';
 
 function App() {
   return <RouterProvider router={routers} />;
