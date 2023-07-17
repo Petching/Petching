9371ef9218e2b13b8bdca091a8f7eb25700e52c9
938938 <938kjh@gmail.com>
2023-07-17 18:26:46 +0900

    [feat] MainPeocock 데이터 반영

--- client/src/Components/Main/MainPeacock.tsx
+++ client/src/Components/Main/MainPeacock.tsx
@@ -2,7 +2,6 @@ import { useGetMainData } from '../../Hook/useGetMainData';
 
 const MainPeacock = () => {
   const { MainData } = useGetMainData();
-
   return (
     <div className="w-full h-full flex flex-col justify-center items-center md:flex-row">
       <div className="w-1/5 -mt-20">
@@ -16,39 +15,22 @@ const MainPeacock = () => {
           자랑하러 가기
         </button>
       </div>
-      <div className="flex">
-        {MainData && (
-          <>
-            <div className="w-10 h-10 md:w-60 md:h-60 relative -top-10 left-32 rounded overflow-hidden hover:scale-105 transition-all">
+      <div className="flex ml-32">
+        {MainData &&
+          MainData.map((img, idx) => (
+            <div
+              className={`w-10 h-10 md:w-60 md:h-60 relative ${
+                idx % 2 === 0 ? '-top-10' : 'top-10'
+              } rounded overflow-hidden hover:scale-105 transition-all`}
+              key={idx}
+            >
               <img
                 loading="lazy"
-                src={`${MainData[0]}`}
+                src={`${img}`}
                 className="w-full h-full object-cover"
               />
             </div>
-            <div className="w-10 h-10 md:w-60 md:h-60 relative top-10 left-20 rounded overflow-hidden hover:scale-105 transition-all">
-              <img
-                loading="lazy"
-                src={`${MainData[1]}`}
-                className="w-full h-full object-cover"
-              />
-            </div>
-            <div className="w-10 h-10 md:w-60 md:h-60 relative -top-10 left-10 rounded overflow-hidden hover:scale-105 transition-all">
-              <img
-                loading="lazy"
-                src={`${MainData[2]}`}
-                className="w-full h-full object-cover"
-              />
-            </div>
-            <div className="w-10 h-10 md:w-60 md:h-60 relative top-10 rounded overflow-hidden hover:scale-105 transition-all">
-              <img
-                loading="lazy"
-                src={`${MainData[3]}`}
-                className="w-full h-full object-cover"
-              />
-            </div>
-          </>
-        )}
+          ))}
       </div>
     </div>
   );
