6996a8e588d088b86653ce4199c1b8fad03c01ce
dogyeong <82639552+yeomdogyeong@users.noreply.github.com>
2023-07-31 17:41:40 +0900

    [refactor]google url secret 적용

--- client/src/Components/Login/Google.tsx
+++ client/src/Components/Login/Google.tsx
@@ -7,11 +7,8 @@ const Google = () => {
     'redirect_uri={https://server.petching.net/login/oauth2/code/google}&' +
     'response_type=token&' +
     'scope=https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile';
-  const loginHandler = () => {
-    window.location.href = googleAuthUrl;
-  };
 
-  return <div onClick={loginHandler}>구글 로그인</div>;
+  return <div>구글 로그인</div>;
 };
 
 export default Google;
