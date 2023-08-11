4fceb8c0d0b4daae2554c35fa4ebeb6b17d6a38c
Sniij <120099321+Sniij@users.noreply.github.com>
2023-08-11 18:07:46 +0900

    [feat] CarePost - MyPage API 테스트 코드 및 문서 작성

--- server/src/main/java/com/Petching/petching/carepost/dto/CarePostDto.java
+++ server/src/main/java/com/Petching/petching/carepost/dto/CarePostDto.java
@@ -103,7 +103,7 @@ public class CarePostDto {
     @Getter @AllArgsConstructor @Builder
     public static class MyPage {
         private String title;
-        private String petSize;
+        private List<String> petSizes;
         private List<String> imgUrls;
         private String profileImgUrl;
         private String nickName;
