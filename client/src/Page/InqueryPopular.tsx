705ac91c6767e767138fae59cadf0cc8196b5754
dogyeong <82639552+yeomdogyeong@users.noreply.github.com>
2023-08-14 10:19:30 +0900

    [feat]dummyData 상위 데이터 필터링

--- client/src/Page/InqueryPopular.tsx
+++ client/src/Page/InqueryPopular.tsx
@@ -2,21 +2,28 @@ import React from 'react';
 import cat from '../Style/greencat.png';
 import questionPicture from '../Style/question.png';
 import InqueryComponent from '../Components/Inquery/InqueryComponent';
-import { useNavigate } from 'react-router-dom';
 import QuestionSection from '../Components/Inquery/QuestionSection';
+import { useNavigate } from 'react-router-dom';
 import { GoToFunction } from '../Util/types';
+import { dummyData } from '../Components/Inquery/dummyData';
 
-const Inquery = () => {
+const InqueryPopular = () => {
   const navi = useNavigate();
-
   const goTo: GoToFunction = (path: string) => {
     navi(path);
   };
 
+  // dummyData 배열을 조회수를 기준으로 내림차순으로 정렬
+  const sortedData = [...dummyData].sort((a, b) => b.조회수 - a.조회수);
+
+  // 상위 10개의 데이터만 가져오기
+  const top10Data = sortedData.slice(0, 10);
+
   return (
     <>
-      <div className=" flex flex-col justify-center items-center h-[60vh] bg-customGreen p-5">
+      <div className=" flex flex-col justify-center items-center  h-[60vh] bg-customGreen p-5">
         <div className=" text-[40px] mb-4 flex items-center p-5">
+          {/* <img src={doctor} /> */}
           자주 묻는 질문
         </div>
         <input
@@ -25,10 +32,13 @@ const Inquery = () => {
         />
         <QuestionSection goTo={goTo} />
       </div>
-      <section className=" flex flex-col items-center justify-center bg-#e0e0e0">
-        <InqueryComponent />
+      <div className="flex justify-center items-center text-2xl mt-4 text-gray-400">
+        조회수 상위 10개의 질문만 표시됩니다
+      </div>
+      <section className="flex flex-col items-center justify-center bg-#e0e0e0">
+        <InqueryComponent data={top10Data} />
       </section>
-      <section className="flex justify-left h-[20vh] bg-gray-100 p-10 inset-x-0 bottom-0">
+      <section className="flex justify-left h-[20vh] bg-gray-100 p-10 overflow-hidden">
         <div className="flex flex-col items-left">
           <div className="text-2xl mb-5">질문에 대한 답을 찾지 못하셨나요?</div>
           <div className="text-lg">1대1 문의하기</div>
@@ -42,4 +52,4 @@ const Inquery = () => {
   );
 };
 
-export default Inquery;
+export default InqueryPopular;
