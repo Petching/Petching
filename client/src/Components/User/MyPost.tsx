ea33145b0e5a8eadd02ff95b228479d9f887cac6
938938 <938kjh@gmail.com>
2023-08-18 19:50:05 +0900

    [feat] 작성한 돌봄리스트 목록 기능 구현

--- client/src/Components/User/MyPost.tsx
+++ client/src/Components/User/MyPost.tsx
@@ -1,13 +1,20 @@
 /* eslint-disable */
 import { useState } from 'react';
-// import { useGetMyPostToCare } from '../../Hook/useGetMyPostToCare';
+import { useGetMyPostToCare } from '../../Hook/useGetMyPostToCare';
+import Card from '../Care/Card';
 
 const MyPost: React.FC<{ userId: string }> = ({ userId }) => {
   const [category, setCategory] = useState<boolean>(true);
-  // 돌봄리스트 / 자랑하기 버튼
-  // 데이터 받아와서 뿌리기(각 게시판의 컴포넌트 사용)
-  // const { MyPostToCare } = useGetMyPostToCare(userId);
-  // console.log(MyPostToCare);
+  const [page, setPage] = useState<number>(1);
+  const { MyPostToCare } = useGetMyPostToCare(userId, page);
+  console.log(MyPostToCare);
+
+  const pageNumber = Array(MyPostToCare?.pageInfo.totalPages)
+    .fill(0)
+    .map((_, index) => index + 1);
+  const goFirstPageHandler = () => {
+    setPage(1);
+  };
 
   return (
     <div className="w-full">
@@ -29,11 +36,48 @@ const MyPost: React.FC<{ userId: string }> = ({ userId }) => {
           자랑하기
         </button>
       </div>
-      <div>
+      {/* <div className="w-full grid gap-4	grid-cols-1 md:grid-cols-3"> */}
+      <div className="flex flex-wrap justify-center">
         {category ? (
-          <>돌봄리스트 / 페이지네이션</>
+          <>
+            {MyPostToCare &&
+              MyPostToCare.data.map((ele, idx) => (
+                // 추후 postId로 감싸는 div 추가할 것
+                <Card
+                  key={idx}
+                  title={ele.title}
+                  locationTag={ele.locationTag}
+                  petSize={ele.petSize}
+                  nickName={ele.nickName}
+                  profileImgUrl={ele.profileImgUrl}
+                  imgUrls={ele.imgUrls}
+                />
+              ))}
+          </>
         ) : (
           <>자랑하기 / 페이지네이션</>
+        )}
+      </div>
+      <div className="mt-10 mb-20 flex justify-center">
+        {pageNumber.length !== 0 ? (
+          pageNumber.map(number => (
+            <button
+              key={number}
+              className={`w-5 h-10 mx-1 rounded ${
+                number === page && 'text-customHoverGreen'
+              } hover:text-customHoverGreen`}
+              onClick={() => setPage(number)}
+            >
+              {number}
+            </button>
+          ))
+        ) : (
+          <button
+            className="w-5 h-10 mx-1 rounded border"
+            onClick={goFirstPageHandler}
+          >
+            1
+          </button>
         )}
       </div>
     </div>
