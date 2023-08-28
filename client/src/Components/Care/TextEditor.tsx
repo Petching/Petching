14888a6d920a8045729537710883e49fc73a3d22
dogyeong <82639552+yeomdogyeong@users.noreply.github.com>
2023-08-28 15:19:50 +0900

    [feat]peacock 자랑하기 게시물 등록 구현

--- client/src/Components/Care/TextEditor.tsx
+++ client/src/Components/Care/TextEditor.tsx
@@ -1,26 +1,31 @@
 import { Editor } from '@toast-ui/react-editor';
 import '@toast-ui/editor/dist/toastui-editor.css';
 import '@toast-ui/editor/dist/i18n/ko-kr';
-import axios from 'axios';
+import { useState, useRef } from 'react';
+import { Axios } from '../../API/api';
 
 export type HookMap = {
   addImageBlobHook?: (blob: Blob | File, callback: HookCallback) => void;
 };
 type HookCallback = (url: string, text?: string) => void;
 
-function TextEditor() {
+const TextEditor = () => {
   const BASE_URL = process.env.REACT_APP_BASE_URL;
+  const [imgUrls, setImgUrls] = useState<string[]>();
+  const editorRef = useRef<Editor | null>(null);
 
   async function uploadImage(blob: any) {
     const files = new FormData();
     files.append('files', blob);
-
+    console.log(blob);
     try {
-      const response = await axios.post(
+      const response = await Axios.post(
         `${BASE_URL}/api/s3/uploads?uploadTo=boards`,
         files,
       );
-      return response.data.url;
+      console.log(response.data);
+      console.log(response.data);
+      return response.data.data[0].uploadFileUrl;
     } catch (error) {
       console.error(error);
     }
@@ -28,23 +33,59 @@ function TextEditor() {
 
   const onUploadImage = async (blob: any, callback: any) => {
     const url = await uploadImage(blob);
+    console.log(url);
     callback(url, 'alt text');
+    //null값 예외처리 추가
+    setImgUrls(prevUrls => (prevUrls ? [...prevUrls, url] : [url]));
     return false;
+  };
+  const handleSubmit = async () => {
+    if (editorRef.current) {
+      const instance = editorRef.current.getInstance();
+      const html = instance.getMarkdown();
+      console.log(html);
+
+      // 이미지 제외한 텍스트 추출
+      const text = html.replace(/!\[[^\]]*\]\([^)]*\)/g, '');
+      console.log(text);
+
+      const requestBody = {
+        title: 'this is title1',
+        content: text,
+        imgUrls: imgUrls,
+      };
+      // Axios를 사용하여 POST 요청 전송
+      try {
+        const response = await Axios.post(`${BASE_URL}/boards`, requestBody);
+        console.log(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
   };
 
   return (
-    <Editor
-      initialValue="hello react editor world!"
-      previewStyle="vertical"
-      height="200px"
-      initialEditType="wysiwyg"
-      useCommandShortcut={false}
-      language="ko-KR"
-      hooks={{
-        addImageBlobHook: onUploadImage,
-      }}
-    />
+    <>
+      <Editor
+        ref={editorRef}
+        initialValue="내용을 입력하세요"
+        previewStyle="vertical"
+        height="500px"
+        useCommandShortcut={false}
+        language="ko-KR"
+        initialEditType="markdown"
+        hooks={{
+          addImageBlobHook: onUploadImage,
+        }}
+      />
+      <button
+        onClick={handleSubmit}
+        className="mt-4 bg-customGreen hover:bg-customHoverGreen text-gray-700 font-bold py-2 px-4 rounded duration-300 hover:scale-110 ease-in-out"
+      >
+        Submit
+      </button>
+    </>
   );
-}
+};
 
 export default TextEditor;
