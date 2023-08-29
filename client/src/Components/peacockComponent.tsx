94ab1c263525b7cf64b84bdcb7ce3c8ec6175934
ggggggggithub <hamtoli5@gmail.com>
2023-08-29 10:01:37 +0900

    [fix]배포오류 수정

--- client/src/Components/peacockComponent.tsx
+++ client/src/Components/peacockComponent.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import pit from '../Style/kakaoLogo.png';
 import { useNavigate } from 'react-router-dom';
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
