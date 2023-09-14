d86d433d229512ca4867515a0b07c1d432cbf778
dogyeong <82639552+yeomdogyeong@users.noreply.github.com>
2023-09-14 13:40:24 +0900

    [innit]sockJs모듈 설치

--- client/src/Components/Chatting/chattingComponent.tsx
+++ client/src/Components/Chatting/chattingComponent.tsx
@@ -1,109 +1,40 @@
-import React, { useState, useEffect, useRef } from 'react';
-import io, { Socket } from 'socket.io-client';
-import Pet from '../../Style/icon_Pet.png';
-
-type ChatEvent = {
-  message: string;
-  room: string;
-  createdAt: string;
-};
+import { Stomp } from '@stomp/stompjs';
+import SockJS from 'sockjs-client';
+import { useState, useRef, useEffect } from 'react';
 
 const ChatComponent: React.FC = () => {
-  const [message, setMessage] = useState<string>('');
-  const [messages, setMessages] = useState<
-    { room: string; message: string; createdAt: Date }[]
-  >([]);
+  const [room, setRoom] = useState('');
+  const inputRef = useRef(null);
 
-  const socketRef = useRef<Socket | null>(null);
-  const [room, setRoom] = useState<string>('defaultRoom');
-  const [inputValue, setInputValue] = useState<string>('');
-  const filteredMessages = messages.filter(msg => msg.room === room);
   useEffect(() => {
-    console.log('socketRef.current:', socketRef.current);
-    socketRef.current = io('http://localhost:4000');
+    const sock = new SockJS('https://petching.net/ws/chat');
+    const stomp = Stomp.over(sock);
 
-    socketRef.current.emit('joinRoom', room);
-
-    socketRef.current?.on('chat', (data: ChatEvent) => {
-      console.log('Received chat event:', data);
-      setMessages(messages => [
-        ...messages,
-        { ...data, createdAt: new Date() },
-      ]);
+    stomp.connect({}, () => {
+      console.log('서버와 연결!');
     });
-    socketRef.current?.on('load_messages', messages => {
-      setMessages(messages);
-    });
-
-    return () => {
-      socketRef.current?.disconnect();
-    };
-  }, [room]);
-
-  const joinRoom = () => {
-    console.log('조인룸');
-    if (socketRef.current && inputValue) {
-      setRoom(inputValue);
-      socketRef.current.emit('joinRoom', inputValue);
-    }
-  };
-
-  const sendMessage = (e: React.FormEvent) => {
-    console.log('메세지');
-    e.preventDefault();
-
-    if (socketRef.current) {
-      socketRef.current.emit('chat', {
-        message,
-        room,
-      });
-
-      setMessage('');
-    }
-  };
+  });
 
   return (
     <div className="flex flex-col w-full h-screen p-4 md:p-8">
-      <h2 className="text-xl mb-2 md:text-2xl">Current room: {room}</h2>
+      <h2 className="text-xl mb-2 md:text-2xl">Current room:</h2>
       <form className="mb-4">
         <input
-          value={inputValue}
-          onChange={e => setInputValue(e.target.value)}
           className="mb-3 p-2 rounded-md border w-full md:w-1/2 md:text-lg"
           placeholder="Enter room"
         />
         <button
           type="button"
-          onClick={joinRoom}
           className="w-full bg-[#99DDCC] text-white p-2 rounded-md hover:bg-[#79C3B1] md:w-1/2 md:text-lg"
         >
           Join Room
         </button>
       </form>
-      <div className="flex-grow overflow-auto mb-4 p-3 bg-white rounded-md">
-        {filteredMessages.map((message, i) => (
-          <div
-            key={i}
-            className="mb-3 p-2 rounded-md space-x-10 bg-gray-100 text-black flex items-center justify-start"
-          >
-            <img
-              className="rounded-full h-8 w-8 mr-3 md:h-10 md:w-10"
-              src={Pet}
-            ></img>
-
-            <p className="text-sm md:text-base">{message.message}</p>
-            <p className="text-xs text-gray-500">
-              {new Date(message.createdAt).toLocaleString()}
-            </p>
-          </div>
-        ))}
-      </div>
-      <form onSubmit={sendMessage} className="flex">
+      <div className="flex-grow overflow-auto mb-4 p-3 bg-white rounded-md"></div>
+      <form className="flex">
         <input
           id="input"
           type="text"
-          value={message}
-          onChange={e => setMessage(e.target.value)}
           className="flex-grow border rounded-l-md p-2 focus:outline-none focus:ring-1 md:text-lg"
         />
         <button
