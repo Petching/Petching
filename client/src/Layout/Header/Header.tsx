97b68cc6d731edbae284754920e0573ba439d903
nowaveosu <nowaveosu@gmail.com>
2023-12-04 16:40:22 +0900

    [design] font 고딕으로 변경, 로고 이미지로 변경

--- client/src/Layout/Header/Header.tsx
+++ client/src/Layout/Header/Header.tsx
@@ -1,5 +1,5 @@
 import { useNavigate } from 'react-router-dom';
-import icon_Pet from '../../Style/icon_Pet.png';
+import logo from '../../Style/petching_logo.png';
 import GNB from './GNB';
 
 const Header = () => {
@@ -10,15 +10,11 @@ const Header = () => {
 
   return (
     <div className="bg-white h-[4.5rem] fixed top-0 left-0 w-full flex justify-between items-center z-50 border text-xl">
-      <div
-        className="flex w-24 h-10 justify-evenly cursor-pointer items-center ml-10"
+      <img
+        src={logo}
+        className="flex w-[6.1rem] justify-evenly cursor-pointer items-center ml-10"
         onClick={toMain}
-      >
-        <div className="w-9 hover:scale-90 overflow-hidden rounded-full mr-2">
-          <img src={icon_Pet} alt="발바닥 모양의 아이콘" />
-        </div>
-        <h1 className="text-3xl text-hoverGreen">펫칭</h1>
-      </div>
+      ></img>
       <GNB />
     </div>
   );
